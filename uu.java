19eb760632334e18d79b1036a4673fd66ccd2abe
nzm-el <nizamel1402@gmail.com>
2024-12-02 11:41:09 +0700

    pertama

--- uu.java
+++ uu.java
@@ -3,6 +3,17 @@ public class uu {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
+        String[] menu = {
+            "Tambah Data Prestasi", 
+            "Tampilkan Semua Prestasi",
+            "Analisis Prestasi Berdasarkan Jenis",
+            "Keluar"          
+        };
+        String dataMahasiswa[][] = new String[3][5]; 
+
+
+
+
+
     }
 }
